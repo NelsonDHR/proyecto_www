eb888be73ecf22fd81eaa14c53e46677a159ae27
jorgeasmz <jorge.eduardo.arias@correounivalle.edu.co>
2023-10-14 15:36:43 -0500

    Exposing models to the admin API

--- backend/splitcount/admin.py
+++ backend/splitcount/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile, Contact, Event, Activity, Participation, Payment
 
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Contact)
+admin.site.register(Event)
+admin.site.register(Activity)
+admin.site.register(Participation)
+admin.site.register(Payment)
