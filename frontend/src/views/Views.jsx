19dd71561fe1df53042436678cc98912d0628530
Nelson Herrera <herrera.nelson@correounivalle.edu.co>
2023-10-18 18:24:25 -0500

    API eventos funciona y frontend tentativo para mostrar eventos

--- frontend/src/views/Views.jsx
+++ frontend/src/views/Views.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import LogIn from "./Auth/LogIn";
 import SignUp from "./Auth/SignUp";
 import Home from "./Home";
+import Events from "./Events/Events";
 
 const Views = () => {
   return (
@@ -10,6 +11,7 @@ const Views = () => {
       <Route path="*" element={<LogIn />} />
       <Route path="/log-in" element={<LogIn />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/events" element={<Events />} />
     </Routes>
   );
 };
