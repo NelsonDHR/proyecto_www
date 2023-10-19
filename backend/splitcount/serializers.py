06e13e1d80722d2626fd1a5890103e7a4212043e
miguelVillaq <125335842+miguelVillaq@users.noreply.github.com>
2023-10-18 22:44:23 -0500

    Crud de actividades desde API únicamente

--- backend/splitcount/serializers.py
+++ backend/splitcount/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Event
+from .models import *
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
 
+class Activity_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = '__all__'
--- backend/splitcount/urls.py
+++ backend/splitcount/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from .views import EventView
+from .views import *
 from rest_framework import routers
 
 router= routers.DefaultRouter()
 router.register(r'event',EventView,'event')
+router.register(r'activities',Activity_view,'activities')
 
 urlpatterns = [
 
