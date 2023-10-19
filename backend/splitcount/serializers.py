59a2ab62458f88aa1822ba9b93650f5844d33453
Nelson Herrera <herrera.nelson@correounivalle.edu.co>
2023-10-19 18:07:45 -0500

    Front and back done

--- backend/splitcount/serializers.py
+++ backend/splitcount/serializers.py
@@ -1,8 +1,15 @@
+from django.utils import timezone
 from rest_framework import serializers
 from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = '__all__'
+        exclude = ['date']  # Excluir el campo 'date' de los campos serializados
+
+    def create(self, validated_data):
+        validated_data['date'] = timezone.now()  # Importa timezone desde django.utils
+        event = Event(**validated_data)
+        event.save()
+        return event
 
