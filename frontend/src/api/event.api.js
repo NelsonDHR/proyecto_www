cc4e6e2c4f87e45cb6b3845dc4f55e3db242dee5
Nelson Herrera <herrera.nelson@correounivalle.edu.co>
2023-10-31 17:10:18 -0500

    Editar done

--- frontend/src/api/event.api.js
+++ frontend/src/api/event.api.js
@@ -7,3 +7,7 @@ export const getAllEvents = () =>{
 export const createEvent = (event) =>{
     return axios.post('http://127.0.0.1:8000/splitcount/api/event/',event)
 }
+
+export const putEvent = (id_event, event) =>{
+    return axios.put(`http://127.0.0.1:8000/splitcount/api/event/${id_event}/`, event)
+}
