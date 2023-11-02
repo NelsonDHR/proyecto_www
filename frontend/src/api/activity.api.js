45e26d5d5cb359bbc834432e9b1eb79b2a0c4204
miguelVillaq <125335842+miguelVillaq@users.noreply.github.com>
2023-11-02 00:07:32 -0500

    Agrego a headers para activity

--- frontend/src/api/activity.api.js
+++ frontend/src/api/activity.api.js
@@ -1,14 +1,20 @@
 import axios from 'axios'
 
+const config = {
+    headers: {
+      'Authorization': `token ${localStorage.getItem('token')}`
+    }
+  }
+
 export const getAllActivities = () =>{
-    return axios.get('http://127.0.0.1:8000/splitcount/api/activities/')
+    return axios.get('http://127.0.0.1:8000/splitcount/api/activities/', config)
 }
 
 export const createActivity = (activity) =>{
-    return axios.post('http://127.0.0.1:8000/splitcount/api/activities/',activity)
+    return axios.post('http://127.0.0.1:8000/splitcount/api/activities/',activity, config)
 }
 
 export const putActivity = (id_activity, activity) =>{
-    return axios.put(`http://127.0.0.1:8000/splitcount/api/activities/${id_activity}/`, activity)
+    return axios.put(`http://127.0.0.1:8000/splitcount/api/activities/${id_activity}/`, activity, config)
 }
 
