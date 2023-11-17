3835971e965407e88edbe5d9ad1696f6a690da72
jorgeasmz <jorge.eduardo.arias@correounivalle.edu.co>
2023-11-16 21:48:19 -0500

    Merged changes

--- frontend/src/api/profile.api.js
+++ frontend/src/api/profile.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const url = import.meta.env.VITE_BACKEND_URL_LOCAL + "/splitcount/user/";
-const url = import.meta.env.VITE_BACKEND_URL_PROD + "/splitcount/user/";
+const url = import.meta.env.VITE_BACKEND_URL_LOCAL + "/splitcount/user/";
+// const url = import.meta.env.VITE_BACKEND_URL_PROD + "/splitcount/user/";
 
 const getConfig = () => ({
   headers: {
